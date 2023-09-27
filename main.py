31544bba45abff3a28c176080a24a0fb7c3e1e81
alexwafula <kwobaalex9@gmail.com>
2023-09-27 21:43:30 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+
+import os
+import pandas as pd
+import glob
+
+
+data_directory = 'data'
+jsonl_files = glob.glob(os.path.join(data_directory, '*.jsonl'))
+
+with pd.ExcelWriter('MASSIVE.xlsx', engine='xlsxwriter') as writer:
+    # Iterate through each JSON Lines file, convert it to an Excel sheet, and save it
+    [pd.read_json(jsonl_file, lines=True).to_excel(writer, sheet_name=os.path.splitext(os.path.basename(jsonl_file))[0], index=False) for jsonl_file in jsonl_files]
+
+# Save the Excel file
+print("Excel file saved as 'MASSIVE.xlsx'")
+
